2532129786ef30df960819c17f757c7af15737e2
CescoIV <crordaz@gmail.com>
2017-12-16 19:30:40 -0600

    working animation on click, works for more than once

--- main.js
+++ main.js
@@ -7,17 +7,31 @@ var contact = document.querySelector('.c')
 //scroll functions
 home.addEventListener('click', (e)=>{
 	e.preventDefault();
-	document.querySelector('.home').scrollIntoView();
+	let item = document.querySelector('.home')
+	item.className = item.className.replace('anim',''); //remove the anim class first
+	console.log(item.className);
+	item.scrollIntoView();
+	item.className += ' anim';// add the anim class after scroll (so the animation plays more than once)
 })
 projects.addEventListener('click', (e)=>{
 	e.preventDefault();
-	document.querySelector('.projects').scrollIntoView();
+	let item = document.querySelector('.projects');
+	item.className = item.className.replace('anim','');
+	item.scrollIntoView();
+	item.className += ' anim';
+
 })
 designs.addEventListener('click', (e)=>{
 	e.preventDefault();
-	document.querySelector('.designs').scrollIntoView();
+	let item = document.querySelector('.designs');
+	item.className = item.className.replace('anim','');
+	item.scrollIntoView();
+	item.className += ' anim';
 })
 contact.addEventListener('click', (e)=>{
 	e.preventDefault();
-	document.querySelector('.contact').scrollIntoView();
+	let item = document.querySelector('.contact');
+	item.className = item.className.replace('anim','');
+	item.scrollIntoView();
+	item.className += ' anim';
 })
