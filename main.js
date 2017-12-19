ce2e4b54337b93064adbed7945519785c8de4a72
CescoIV <crordaz@gmail.com>
2017-12-18 20:38:32 -0600

    added tons of images, fixing animation for gallery

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //button selectors
-var home = document.querySelector('.h');
-var projects = document.querySelector('.p');
-var designs = document.querySelector('.d');
-var contact = document.querySelector('.c')
+let home = document.querySelector('.h');
+let projects = document.querySelector('.p');
+let designs = document.querySelector('.d');
+let contact = document.querySelector('.c')
 
 //scroll functions
 home.addEventListener('click', (e)=>{
@@ -34,4 +34,29 @@ contact.addEventListener('click', (e)=>{
 	item.className = item.className.replace('anim','');
 	item.scrollIntoView();
 	item.className += ' anim';
+})
+
+//image gallery functionality
+
+let forward = document.querySelector('.forward');
+let back = document.querySelector('.back')
+let count = 1;
+let max_images= 6; //change as you add more images
+
+//forward button script
+forward.addEventListener('click', (e)=>{
+	e.preventDefault();
+	count === max_images ? count=1 : count+=1;
+	let img = document.querySelector('#current-gal-display');
+	img.className = '';
+	img.setAttribute('src', `./images/showcase${count}.png`);
+	img.className = 'anim';
+
+
+})
+//back button script
+back.addEventListener('click', ()=>{
+	count === 1 ? count = max_images : count-=1;
+	let img = document.querySelector('#current-gal-display');
+	img.setAttribute('src', `./images/showcase${count}.png`);
 })
